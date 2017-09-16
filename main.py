66592052647b8d7067f875fc1fcdfa6a83ecfd69
dongqi zhang <zdq123.hn@163.com>
2017-09-16 21:28:38 +0800

    add save

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ pull
 '''
 
 from optparse import OptionParser
-import lib.docker_hub as docker_hub
+import lib.hub as docker_hub
+import json
+
+def store(obj):
+    f = open('out/repos.txt', 'w')
+    f.write(obj)
+    f.flush()
+    f.close()
 
 def main():
     parser = OptionParser()
@@ -11,10 +18,12 @@ def main():
     parser.add_option("-t", "--tag", action="store_true", dest="tag")
     parser.add_option("-p", "--pull", action="store_true", dest="pull")
     parser.add_option("-s", "--inspect", action="store_true", dest="inspect")
+    parser.add_option("-S", "--save", action="store_true", dest="save")
     (options, args) = parser.parse_args()
     get_meta = options.info
     get_tags = options.tag
     pull_image = options.pull
+    save = options.save
     if pull_image:
         inspect_image = options.inspect
 
@@ -26,7 +35,8 @@ def main():
         if get_tags:
             docker_hub.get_tags_of(repo)
 
-    print repos[1]["name"]
+    if save:
+        store(json.dumps(repos, indent=4))
 
 if __name__ == "__main__":
     main()
